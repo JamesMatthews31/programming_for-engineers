8115480b8fec29031667b1ce906f83e39a6a877d
JamesMatthews31 <5297083@student.gloscol.ac.uk>
2026-05-12 15:53:27 +0100

    Commit so i can access at college
    Reworking RMS calc

--- PowerQualityWaveformAnalyser/src/waveform.c
+++ PowerQualityWaveformAnalyser/src/waveform.c
@@ -1,24 +1,26 @@
 #include "waveform.h"
 #include <stdio.h>
+#include <math.h>
 
+float computeRMS(waveform *waveformLog[], float *RMSValues[], int sampleCount){
 
-float computeRMS(waveform *waveformLog[]){
     float phaseARMS, phaseBRMS, phaseCRMS = 0;
-    int samples = 0;
     waveform *readingPointer = waveformLog;
 
-    while(readingPointer->timestamp >= 0){
+    for (int i = 0; i = sampleCount; i++){
         // Adds the square of the current phase voltage to each counter until the timesamp starts returning a negative value and hence is out of range
         phaseARMS += readingPointer->phaseAVoltage*readingPointer->phaseAVoltage;
         phaseBRMS += readingPointer->phaseBVoltage*readingPointer->phaseBVoltage;
         phaseCRMS += readingPointer->phaseCVoltage*readingPointer->phaseCVoltage;
         // Increments the reading pointer and sample count each time
         readingPointer++;
-        samples++;
     }
 
+    *RMSValues[0] = sqrt(phaseARMS / sampleCount);
+    *RMSValues[1] = sqrt(phaseBRMS / sampleCount);
+    *RMSValues[2] = sqrt(phaseCRMS / sampleCount);
 
-    
+    return;
 }
 
 float computeP2P(waveform waveformLog[]); 
