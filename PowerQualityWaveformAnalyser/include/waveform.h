26ca58413aeb7225461def633b50cb9fe2cc506b
deckedbyschmeck <jamesnmatthews1@gmail.com>
2026-05-12 11:18:29 +0100

    Waveform.h implemented
    Added body of waveform.h

--- PowerQualityWaveformAnalyser/include/waveform.h
+++ PowerQualityWaveformAnalyser/include/waveform.h
@@ -1,5 +1,32 @@
 // Header file for waveform data structure and calculation functions
 
+// Start of header guard
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
+
+// Waveform structure definition
+
 typedef struct{
-    
-}
+    float timestamp;
+    float phaseAVoltage;
+    float phaseBVoltage;
+    float phaseCVoltage;
+    float lineCurrent;
+    float frequency;
+    float powerFactor;
+    float thdPercent;
+} waveform;
+
+// Function prototypes for waveform calculations
+
+float computeRMS(waveform waveformLog[]); 
+
+float computeP2P(waveform waveformLog[]); 
+
+float computeDCOffset(float phaseRMSVoltages[]); 
+
+float computeClipping(waveform waveformLog[]); 
+
+
+#endif
+// End of header guard
